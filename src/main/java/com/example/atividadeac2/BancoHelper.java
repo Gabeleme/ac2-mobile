49158c17a546a4e81605d3af1f262d29f01915d4
Gabriela Leme <94871691+Gabeleme@users.noreply.github.com>
2025-10-16 22:16:35 -0300

    Subindo os códigos feitos em Aula

---
+++ src/main/java/com/example/atividadeac2/BancoHelper.java
@@ -0,0 +1,83 @@
+package com.example.atividadeac2;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class BancoHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "filmesbanco.db";
+    private static final int DATABASE_VERSION = 1;
+
+    private static final String COLUMN_ID = "id";
+    private static final String TABLE_NAME = "filmes";
+
+    private static final String COLUMN_TITULO = "titulo";
+    private static final String COLUMN_DIRETOR = "diretor";
+
+    private static final String COLUMN_ANOLANCAMENTO = "ano";
+
+    private static final String COLUMN_NOTA = "nota";
+
+    private static final String COLUMN_GENERO = "genero";
+
+    private static final String COLUMN_VIUCINEMA ="viucinema";
+
+    public BancoHelper(Context context){
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db){
+        String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + "("
+                + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + COLUMN_TITULO + " TEXT, "
+                + COLUMN_DIRETOR + " TEXT, "
+                + COLUMN_ANOLANCAMENTO + " INTEGER, "
+                + COLUMN_NOTA + " INTEGER, "
+                + COLUMN_VIUCINEMA+ " INTEGER, "
+                + COLUMN_GENERO + " TEXT)";
+        db.execSQL(CREATE_TABLE);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+    public String InserirFilmes (int id, String titulo, String diretor, int ano, int nota, String genero, int viucinema) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_TITULO, titulo);
+        values.put(COLUMN_DIRETOR, diretor);
+        values.put(COLUMN_ANOLANCAMENTO, ano);
+        values.put(COLUMN_NOTA, nota);
+        values.put(COLUMN_GENERO, genero);
+        values.put(COLUMN_VIUCINEMA, viucinema);
+        return db.insert(TABLE_NAME,null, values);
+    }
+
+    public String atualizarFilmes (int id, String titulo, String diretor, int ano, int nota, String genero, int viucinema){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_TITULO, titulo);
+        values.put(COLUMN_DIRETOR, diretor);
+        values.put(COLUMN_ANOLANCAMENTO, ano);
+        values.put(COLUMN_NOTA, nota);
+        values.put(COLUMN_GENERO, genero);
+        values.put(COLUMN_VIUCINEMA, viucinema);
+        return db.update(TABLE_NAME, values, COLUMN_ID + "=?", new String[]{String.valueOf(id)});
+    }
+    public Cursor listaFilmes(){
+        SQLiteDatabase db = this.getReadableDatabase();
+        return db.rawQuery("SELECT * FROM " + TABLE_NAME, null);
+    }
+
+    public Cursor listarFilmesPorGenero(String genero){
+        SQLiteDatabase db = this.getReadableDatabase();
+        return db.rawQuery("SELECT * FROM " + TABLE_NAME,+ " WHERE genero = ?", new String[]{genero});
+    }
+}
---
+++ src/main/java/com/example/atividadeac2/MainActivity.java
@@ -0,0 +1,92 @@
+package com.example.atividadeac2;
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editTextTitulo, editTextano, editTextdiretor, editTextnota, editTextgenero;
+
+    Button buttonsalvar;
+
+    CheckBox checkBoxcinema;
+
+    ListView listar;
+
+    ArrayAdapter<String> adapter;
+    ArrayList<String> listaFilmes;
+
+    ArrayList<Integer> listaIds;
+
+    BancoHelper databaseHelper;
+
+    ListView listViewFilmes;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        editTextTitulo = findViewById(R.id.editTextTitulo);
+        editTextano = findViewById(R.id.editTextano);
+        editTextdiretor = findViewById(R.id.editTextdiretor);
+        editTextnota = findViewById(R.id.editTextnota);
+        editTextgenero = findViewById(R.id.editTextgenero);
+        listar = findViewById(R.id.listar);
+        checkBoxcinema = findViewById(R.id.checkBoxcinema);
+
+        private void carregarFilme() {
+            Cursor cursor = databaseHelper.listaFilmes();
+            listaFilmes = new ArrayList<>();
+            listaIds = new ArrayList<>();
+            if (cursor.moveToFirst()) {
+                do{
+                    int id = cursor.getInt(0);
+                    String titulo = cursor.getString(1);
+                    String diretor = cursor.getString(2);
+                    String ano = cursor.getString(3);
+                    String nota = cursor.getString(4);
+                    String genero = cursor.getString(5);
+
+                    listaFilmes.add(id + " - " + titulo + " - " + diretor + " - " + ano + " - " + nota + " - " + genero);
+                }while (cursor.moveToNext());
+            }
+
+
+        buttonsalvar.setOnClickListener(v -> {
+            String titulo = editTextTitulo.getText().toString();
+            String diretor = editTextdiretor.getText().toString();
+            String ano = editTextano.getText().toString();
+            String nota = editTextnota.getText().toString();
+            String genero = editTextgenero.getText().toString();
+
+
+        }
+    }
+
+
+
+
+}

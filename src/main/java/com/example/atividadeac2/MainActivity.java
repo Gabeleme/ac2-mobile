d733e726ee07c75632cbfe95bcf89bbd38f3f6a6
Gabriela Leme <94871691+Gabeleme@users.noreply.github.com>
2025-10-17 13:11:33 -0300

    Organização dos dados

--- src/main/java/com/example/atividadeac2/MainActivity.java
+++ src/main/java/com/example/atividadeac2/MainActivity.java
@@ -36,8 +36,6 @@ public class MainActivity extends AppCompatActivity {
     ListView listViewFilmes;
 
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,15 +45,29 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
 
-        editTextTitulo = findViewById(R.id.editTextTitulo);
-        editTextano = findViewById(R.id.editTextano);
-        editTextdiretor = findViewById(R.id.editTextdiretor);
-        editTextnota = findViewById(R.id.editTextnota);
-        editTextgenero = findViewById(R.id.editTextgenero);
-        listar = findViewById(R.id.listar);
-        checkBoxcinema = findViewById(R.id.checkBoxcinema);
+            editTextTitulo = findViewById(R.id.editTextTitulo);
+            editTextano = findViewById(R.id.editTextano);
+            editTextdiretor = findViewById(R.id.editTextdiretor);
+            editTextnota = findViewById(R.id.editTextnota);
+            editTextgenero = findViewById(R.id.editTextgenero);
+            listar = findViewById(R.id.listar);
+            checkBoxcinema = findViewById(R.id.checkBoxcinema);
+            buttonsalvar = findViewById(R.id.buttonsalvar);
+
+            buttonsalvar.setOnClickListener(v -> {
+            String titulo = editTextTitulo.getText().toString();
+            String diretor = editTextdiretor.getText().toString();
+            String ano = editTextano.getText().toString();
+            String nota = editTextnota.getText().toString();
+            String genero = editTextgenero.getText().toString();
+            int viuCinema = checkBoxcinema.isChecked() ? 1 : 0;
+
+            databaseHelper.inserirFilme(titulo, diretor, ano, nota, genero, viuCinema);
+            carregarFilmes();
+        });
+ }
+       
 
         private void carregarFilme() {
             Cursor cursor = databaseHelper.listaFilmes();
@@ -70,23 +82,12 @@ public class MainActivity extends AppCompatActivity {
                     String nota = cursor.getString(4);
                     String genero = cursor.getString(5);
 
+                    listaIds.add(id);
                     listaFilmes.add(id + " - " + titulo + " - " + diretor + " - " + ano + " - " + nota + " - " + genero);
                 }while (cursor.moveToNext());
-            }
-
-
-        buttonsalvar.setOnClickListener(v -> {
-            String titulo = editTextTitulo.getText().toString();
-            String diretor = editTextdiretor.getText().toString();
-            String ano = editTextano.getText().toString();
-            String nota = editTextnota.getText().toString();
-            String genero = editTextgenero.getText().toString();
-
-
-        }
+            }  
+     adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listaFilmes);
+    listar.setAdapter(adapter);
     }
-
-
-
 
 }

1d7b11bf41415348756832e52675481d759dd180
Gabriela Leme <94871691+Gabeleme@users.noreply.github.com>
2025-10-23 14:20:43 -0300

    adicionando comentários

--- src/main/java/com/example/atividadeac2/MainActivity.java
+++ src/main/java/com/example/atividadeac2/MainActivity.java
@@ -17,23 +17,25 @@ import androidx.core.view.WindowInsetsCompat;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    
+    //declaração do campo de entrada
     EditText editTextTitulo, editTextano, editTextdiretor, editTextnota, editTextgenero;
-
+    
+    // declaração do botão
     Button buttonsalvar;
 
+    // declaração da Checkbox
     CheckBox checkBoxcinema;
 
+    //declaração do campo que lista os filmes
     ListView listar;
 
     ArrayAdapter<String> adapter;
-    ArrayList<String> listaFilmes;
+    ArrayList<String> listaFilmes; //lista que armazena os dados dos filmes
+    ArrayList<Integer> listaIds; //lista que armazena os ids
 
-    ArrayList<Integer> listaIds;
-
-    BancoHelper databaseHelper;
-
-    ListView listViewFilmes;
+    BancoHelper databaseHelper; //declaração do banco
+    ListView listViewFilmes; 
 
 
     @Override
@@ -46,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
 
+            //conecta as variaveis aos componentes 
             editTextTitulo = findViewById(R.id.editTextTitulo);
             editTextano = findViewById(R.id.editTextano);
             editTextdiretor = findViewById(R.id.editTextdiretor);
@@ -55,6 +58,8 @@ public class MainActivity extends AppCompatActivity {
             checkBoxcinema = findViewById(R.id.checkBoxcinema);
             buttonsalvar = findViewById(R.id.buttonsalvar);
 
+            //quando clicar em salvar vai capturar os dados digitados nos compos
+            //verifica a checkbox
             buttonsalvar.setOnClickListener(v -> {
             String titulo = editTextTitulo.getText().toString();
             String diretor = editTextdiretor.getText().toString();
@@ -63,12 +68,14 @@ public class MainActivity extends AppCompatActivity {
             String genero = editTextgenero.getText().toString();
             int viuCinema = checkBoxcinema.isChecked() ? 1 : 0;
 
+            //insere os dados no banco de dados 
             databaseHelper.inserirFilme(titulo, diretor, ano, nota, genero, viuCinema);
-            carregarFilmes();
+            carregarFilmes(); //carrega a lista de filmes
         });
  }
        
-
+        // Método que carrega os filmes do banco
+        //percorre o registro e extrai os dados de cada uma das colunas                                     
         private void carregarFilme() {
             Cursor cursor = databaseHelper.listaFilmes();
             listaFilmes = new ArrayList<>();
@@ -82,12 +89,13 @@ public class MainActivity extends AppCompatActivity {
                     String nota = cursor.getString(4);
                     String genero = cursor.getString(5);
 
-                    listaIds.add(id);
+                    //adiciona na lista e cria um string de tudo 
+                    listaIds.add(id); 
                     listaFilmes.add(id + " - " + titulo + " - " + diretor + " - " + ano + " - " + nota + " - " + genero);
                 }while (cursor.moveToNext());
             }  
-     adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listaFilmes);
-    listar.setAdapter(adapter);
+     adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listaFilmes); //cria o que vai mostrar os filmes na tela
+    listar.setAdapter(adapter); //exibe os filmes na tela
     }
 
 }

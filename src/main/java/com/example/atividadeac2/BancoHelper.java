4d101b9515a6da23174db62bf50766ffc2edc1ef
Gabriela Leme <94871691+Gabeleme@users.noreply.github.com>
2025-10-23 14:08:06 -0300

    adicionando comentários

--- src/main/java/com/example/atividadeac2/BancoHelper.java
+++ src/main/java/com/example/atividadeac2/BancoHelper.java
@@ -7,27 +7,28 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class BancoHelper extends SQLiteOpenHelper {
+    
+    //Nome do banco de dados
     private static final String DATABASE_NAME = "filmesbanco.db";
-    private static final int DATABASE_VERSION = 1;
+    private static final int DATABASE_VERSION = 1; //versão do banco de dados
 
+    //definindo o nome da tabela e o nome das colunas 
     private static final String COLUMN_ID = "id";
     private static final String TABLE_NAME = "filmes";
-
     private static final String COLUMN_TITULO = "titulo";
     private static final String COLUMN_DIRETOR = "diretor";
-
     private static final String COLUMN_ANOLANCAMENTO = "ano";
-
     private static final String COLUMN_NOTA = "nota";
-
     private static final String COLUMN_GENERO = "genero";
-
     private static final String COLUMN_VIUCINEMA ="viucinema";
-
+    
+    //contrutor da classe que passa o nome da tabela e a versão para a super classe
+    //inicializa o helper para gerenciar a criação e atualização do banco
     public BancoHelper(Context context){
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
+    //criação da tabela e das colunas 
     @Override
     public void onCreate(SQLiteDatabase db){
         String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + "("
@@ -38,18 +39,19 @@ public class BancoHelper extends SQLiteOpenHelper {
                 + COLUMN_NOTA + " INTEGER, "
                 + COLUMN_VIUCINEMA+ " INTEGER, "
                 + COLUMN_GENERO + " TEXT)";
-        db.execSQL(CREATE_TABLE);
+        db.execSQL(CREATE_TABLE); //quando executada cria a tabela no banco 
 
     }
 
+    
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME); //apaga uma tabela e cria uma nova
         onCreate(db);
     }
 
     public long InserirFilmes (String titulo, String diretor, int ano, int nota, String genero, int viucinema) {
-        SQLiteDatabase db = this.getWritableDatabase();
+        SQLiteDatabase db = this.getWritableDatabase(); 
         ContentValues values = new ContentValues();
         values.put(COLUMN_TITULO, titulo);
         values.put(COLUMN_DIRETOR, diretor);
@@ -69,21 +71,22 @@ public class BancoHelper extends SQLiteOpenHelper {
         values.put(COLUMN_NOTA, nota);
         values.put(COLUMN_GENERO, genero);
         values.put(COLUMN_VIUCINEMA, viucinema);
-        return db.update(TABLE_NAME, values, COLUMN_ID + "=?", new String[]{String.valueOf(id)});
+        return db.update(TABLE_NAME, values, COLUMN_ID + "=?", new String[]{String.valueOf(id)}); //atualiza o filme conforme o id passado
     }
     public Cursor listaFilmes(){
         SQLiteDatabase db = this.getReadableDatabase();
-        return db.rawQuery("SELECT * FROM " + TABLE_NAME, null);
+        return db.rawQuery("SELECT * FROM " + TABLE_NAME, null); //consulta todos os filmes 
     }
 
     public Cursor listarFilmesPorGenero(String genero){
         SQLiteDatabase db = this.getReadableDatabase();
-        return db.rawQuery("SELECT * FROM " + TABLE_NAME + " WHERE genero = ?", new String[]{genero});
+        return db.rawQuery("SELECT * FROM " + TABLE_NAME + " WHERE genero = ?", new String[]{genero}); //lista o filme conforme o genero informado
     }
 
-     public int excluirFilme(int id) {
+     public int excluirFilme(int id) { 
         SQLiteDatabase db = this.getWritableDatabase();
-        return db.delete(TABLE_NAME, COLUMN_ID + "=?", new String[]{String.valueOf(id)});
+        return db.delete(TABLE_NAME, COLUMN_ID + "=?", new String[]{String.valueOf(id)}); //exclui um filme pelo id passado 
     }
 }
+
 
